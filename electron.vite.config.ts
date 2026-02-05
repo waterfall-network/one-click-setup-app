50878a48750973498198175754001414ba13fe18
Oleksandr Nashyvan <on@waterfall.foundation>
2026-02-06 01:19:59 +0200

    update electron to 29

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,9 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
         external: ['better-sqlite3', 'bcrypto']
@@ -16,7 +15,6 @@ export default defineConfig({
     }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
         input: {
